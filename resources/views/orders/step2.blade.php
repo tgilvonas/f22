8008840f66760e1d9e8e58631e441f700fbcc623
tgilvonas <tgilvonas@gmail.com>
2021-03-08 20:41:48 +0200

    Step 2 is in progress

--- resources/views/orders/step2.blade.php
+++ resources/views/orders/step2.blade.php
@@ -8,13 +8,13 @@
         @csrf
         <div>
             @foreach($orderTypes as $orderType)
-                <div><input type="radio" name="order_type" value="{{ $orderType->id }}" /></div>
+                <div><input type="radio" name="order_type" value="{{ $orderType->id }}" class="js-order-type-input" /></div>
             @endforeach
         </div>
         <div class="row">
             @foreach($orderTypes as $orderType)
                 <div class="col-xl-4">
-                    <div class="print-type-box">
+                    <div class="print-type-box js-order-type-box" data-id="{{ $orderType->id }}">
                         <div class="title text-center text-uppercase font-weight-bold">
                             {{ $orderType->title }}
                         </div>
@@ -22,7 +22,7 @@
                             {{ $orderType->description }}
                         </div>
                         <div class="btn-wrapper text-center">
-                            <button type="button" class="js-select-print_type btn btn-primary text-uppercase">{{ __('Renkuosi') }}</button>
+                            <button type="button" class="js-select-print_type btn btn-primary text-uppercase font-weight-bold">{{ __('Renkuosi') }}</button>
                         </div>
                     </div>
                 </div>
@@ -30,14 +30,24 @@
         </div>
         <div>
             @foreach($printFormats as $printFormat)
-                <div><input type="radio" name="print_format" value="{{ $printFormat->id }}" /></div>
+                <div><input type="radio" name="print_format" value="{{ $printFormat->id }}" class="js-print-format-input" /></div>
             @endforeach
         </div>
         <div class="row">
             <div class="col-xl-5">
                 <div class="row">
+                    <div class="col-xl-12">
+                        {{ __('Pasirinkite dydį A4 arba A5') }}*
+                    </div>
                     @foreach($printFormats as $printFormat)
-                        <div class="col-xl-6">{{ $printFormat->measurements }}</div>
+                        <div class="col-xl-6">
+                            <div class="print-format-box text-center js-print-format-box" data-id="{{ $printFormat->id }}">
+                                <img src="/img/print-formats/{{ strtolower($printFormat->title).'.png' }}" alt="{{ $printFormat->title }}" style="width: 146px; height: 200px;" />
+                                <div class="font-weight-bold">
+                                    {{ $printFormat->measurements }}
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                 </div>
             </div>
